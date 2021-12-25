aea249f7590aaad66bc11788e5b018e96e7af634
suv27 <starlinu33@gmail.com>
2021-12-24 23:16:19 -0500

    bst: add() and remove() functions

--- bst.js
+++ bst.js
@@ -49,5 +49,42 @@ class BST {
         return false + ': data doen\'t exist on the tree';
     }
 
-}
+    add(data) {
+        let node = this.root;
+        if(node === null) node = new Node(data);
+        else {
+            const searchTree = (node) => {
+                if(data < node.data) {
+                    if(node.left === null) {
+                        node.left = new Node(data);
+                    } else if(node.left !== null) return searchTree(node.left);
+                } else if(data > node.data) {
+                    if(node.right === null) {
+                        node.right = new Node(data);
+                    } else if(node.right !== null) return searchTree(node.right);
+                } else return null;
+            }
+        };
 
+        return searchTree(node);
+    }
+
+    remove(data) {
+        const removeNode = (node, data) => {
+            if(node.data === null) return null + ': Tree is empty.';
+            if(data == node.data) {
+                if(node.left == null && node.right == null) return null;
+                if(node.left == null) return node.right;
+                if(node.right == null) return node.left;
+
+                let tempNode = node.right;
+                while(tempNode.left !== null) tempNode = tempNode.left;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+            } else if(data < node.data) node.left = removeNode(node.left, data);
+            else node.right = removeNode(node.right, data);
+        }
+
+        this.root = removeNode(this.root, data);
+    }
+}
